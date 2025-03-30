0c3cefb4c827325dbb76c21e7567052dafa76883
Yuviellia <julgol2002@gmail.com>
2025-03-30 16:06:53 +0200

    feat(orm): add Tag ORM entity

---
+++ src/Entity/Tag.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="tags")
+ */
+class Tag {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * Many Tags belong to One User.
+     * @ORM\ManyToOne(targetEntity="App\Entity\User")
+     * @ORM\JoinColumn(name="iduser", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function __construct() {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getId(): ?int { return $this->id; }
+
+    public function getUser(): ?User { return $this->user; }
+    public function setUser(User $user): self {
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getName(): ?string { return $this->name;  }
+    public function setName(string $name): self {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface { return $this->createdAt; }
+    public function setCreatedAt(\DateTimeInterface $createdAt): self {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+}
