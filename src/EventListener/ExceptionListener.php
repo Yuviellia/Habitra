7a4d663516439c3d74ae26f3c5ee5cbea361f419
Yuviellia <julgol2002@gmail.com>
2025-04-21 18:47:47 +0200

    feat: add global exception listener

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,19 @@
+<?php
+// src/EventListener/ExceptionListener.php
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener {
+    public function onKernelException(ExceptionEvent $event): void {
+        $exception = $event->getThrowable();
+        $status = $exception instanceof HttpExceptionInterface ? $exception->getStatusCode() : 500;
+
+        $event->setResponse(new JsonResponse([
+            'error' => $exception->getMessage(),
+            'status' => $status
+        ], $status));
+    }
+}
